42a9fc432075989ef019e54e5d97bda7617d3f1f
Tommy Dunn <tdunn@Tommy-Dunn-MBP.home>
2015-12-11 00:48:18 -0500

    wip

--- js/holiday.js
+++ js/holiday.js
@@ -1,57 +1,57 @@
 var holiday = (function(){
     var self;
-	
+
     return {
-		
+
         init: function(){
-            self    = this;		
-			
-			self.items();	
+            self    = this;
+
+			self.items();
 			self.preload();
         },
-		
+
 		items: function(){
 			$('.grid .item').on('click', function(){
-				
-				$('.hero img').removeClass('active');
-				$('.hero img').eq($(this).index() + 1).addClass('active');
-				
-				$('.hero .ribbon').text($(this).data('title'));
-				
-				$('html, body').animate({
-				      scrollTop: $('.hero').offset().top
-				}, 200);
-				
+
+				// $('.hero img').removeClass('active');
+				// $('.hero img').eq($(this).index() + 1).addClass('active');
+				//
+				// $('.hero .ribbon').text($(this).data('title'));
+				//
+				// $('html, body').animate({
+				//       scrollTop: $('.hero').offset().top
+				// }, 200);
+
 			});
-			
+
 			$('.grid .item').on({'mouseover' : function(){
 				$(this).addClass('active');
-				
+
 				if(Modernizr.touch){
 					$('.hero img').removeClass('active');
 					$('.hero img').eq($(this).index() + 1).addClass('active');
-				
+
 					$('.hero .ribbon').text($(this).data('title'));
-				
+
 					$('html, body').animate({
 					      scrollTop: $('.hero').offset().top
 					}, 200);
 				}
-				
+
 			}, 'mouseout' : function(){
 				$(this).removeClass('active');
 			}})
 		},
-		
+
 		preload: function(){
 			$('.hero img').first().load(function() {
 		        $('.hero .overlay').fadeOut();
 		    });
-			
+
 			var item
-			
+
 			$('img.lazy').each(function(i){
-				$(this).attr('src', $(this).data('src'));	
+				$(this).attr('src', $(this).data('src'));
 			});
 		}
     };
